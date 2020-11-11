bf429d55d2f87006361f099db0c5ca464db4464c
Peter Pfeufer <info@ppfeufer.de>
2020-11-11 08:49:32 +0100

    v0.2.0

--- sovtimer/__init__.py
+++ sovtimer/__init__.py
@@ -6,5 +6,5 @@ a couple of variable to use throughout the app
 
 default_app_config: str = "sovtimer.apps.AaSovtimerConfig"
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __title__ = "Sovereignty Timer"
