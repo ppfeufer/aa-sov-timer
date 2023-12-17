589aed5e41bbc98aa7fe3ce58e00e67f770c124a
Peter Pfeufer <info@ppfeufer.de>
2023-12-17 13:15:54 +0100

    [CHANGE] Move app version to `__init__.py`

--- sovtimer/__init__.py
+++ sovtimer/__init__.py
@@ -2,13 +2,8 @@
 App init
 """
 
-# Standard Library
-from importlib import metadata
-
 # Django
 from django.utils.translation import gettext_lazy as _
 
-__version__ = metadata.version(distribution_name="aa-sov-timer")
+__version__ = "1.12.3"
 __title__ = _("Sovereignty Timers")
-
-del metadata
