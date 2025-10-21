ccf2de8fa608599fb080d70798a5d18e832ffb01
Peter Pfeufer <info@ppfeufer.de>
2025-10-21 22:13:44 +0200

    [REMOVE] Unused variable

--- sovtimer/__init__.py
+++ sovtimer/__init__.py
@@ -14,6 +14,5 @@ __esi_compatibility_date__ = "2025-09-30"
 __package_name__ = "aa-sov-timer"
 __app_name__ = "sovtimer"
 __app_name_verbose__ = "AA Sovereignty Timer"
-__app_name_useragent__ = "AA-Sovereignty-Timer"
 
 __github_url__ = f"https://github.com/ppfeufer/{__package_name__}"
