628aa38ebf552e60d05e1e2e47a27aabb732a120
Peter Pfeufer <info@ppfeufer.de>
2025-11-10 13:24:29 +0100

    [CHANGE] switch to DataTables 2

--- sovtimer/static/sovtimer/js/aa-sov-timer.js
+++ sovtimer/static/sovtimer/js/aa-sov-timer.js
@@ -1,4 +1,4 @@
-/* global sovtimerJsSettingsDefaults, sovtimerJsSettingsOverride, moment, objectDeepMerge, fetchGet */
+/* global sovtimerJsSettingsDefaults, sovtimerJsSettingsOverride, moment, objectDeepMerge, fetchGet, DataTable */
 
 $(document).ready(() => {
     'use strict';
@@ -17,6 +17,24 @@ $(document).ready(() => {
         campaignsTotal: $('.aa-sovtimer-campaigns-total'),
         campaignsUpcoming: $('.aa-sovtimer-campaigns-upcoming'),
         campaignsActive: $('.aa-sovtimer-campaigns-active')
+    };
+
+    /**
+     * Remove search from column control.
+     *
+     * @param {Array} columnControl
+     * @param {int} index
+     * @return {Array}
+     * @private
+     */
+    const _removeSearchFromColumnControl = (columnControl, index = 1) => {
+        const cc = JSON.parse(JSON.stringify(columnControl));
+
+        if (cc[index]) {
+            cc[index].content = [];
+        }
+
+        return cc;
     };
 
     /**
@@ -58,159 +76,156 @@ $(document).ready(() => {
     fetchGet({url: sovtimerSettings.url.ajaxUpdate})
         .then((tableData) => {
             if (tableData) {
+                console.log($('.navbar.fixed-top').height());
                 // Destroy any existing DataTable
                 // if ($.fn.dataTable.isDataTable(sovCampaignTable)) {
                 //     sovCampaignTable.DataTable().destroy();
                 // }
 
                 // Create the DataTable
-                sovCampaignTable.DataTable({
+                const dt = new DataTable(sovCampaignTable, { // eslint-disable-line no-unused-vars
                     language: sovtimerSettings.dataTables.language,
                     data: tableData,
+                    dom: sovtimerSettings.dataTables.dom,
+                    ordering: sovtimerSettings.dataTables.ordering,
+                    columnControl: sovtimerSettings.dataTables.columnControl,
                     columns: [
                         // Column: 0
                         {
-                            data: 'event_type'
+                            // data: 'solar_system_name_html'
+                            data: {
+                                display: (data) => {
+                                    return data.solar_system_name_html;
+                                },
+                                sort: (data) => {
+                                    return data.solar_system_name;
+                                },
+                                filter: (data) => {
+                                    return data.solar_system_name;
+                                }
+                            }
                         },
 
                         // Column: 1
                         {
-                            data: 'solar_system_name_html'
+                            // data: 'constellation_name_html'
+                            data: {
+                                display: (data) => {
+                                    return data.constellation_name_html;
+                                },
+                                sort: (data) => {
+                                    return data.constellation_name;
+                                },
+                                filter: (data) => {
+                                    return data.constellation_name;
+                                }
+                            }
                         },
 
                         // Column: 2
                         {
-                            data: 'constellation_name_html'
+                            // data: 'region_name_html'
+                            data: {
+                                display: (data) => {
+                                    return data.region_name_html;
+                                },
+                                sort: (data) => {
+                                    return data.region_name;
+                                },
+                                filter: (data) => {
+                                    return data.region_name;
+                                }
+                            }
                         },
 
                         // Column: 3
                         {
-                            data: 'region_name_html'
+                            // data: 'defender_name_html'
+                            data: {
+                                display: (data) => {
+                                    return data.defender_name_html;
+                                },
+                                sort: (data) => {
+                                    return data.defender_name;
+                                },
+                                filter: (data) => {
+                                    return data.defender_name;
+                                }
+                            }
                         },
 
                         // Column: 4
                         {
-                            data: 'defender_name_html'
+                            data: 'adm'
                         },
 
                         // Column: 5
                         {
-                            data: 'adm'
-                        },
-
-                        // Column: 6
-                        {
-                            data: 'start_time',
-                            render: {
-                                _: (data) => {
-                                    return data === null ? '' : moment(data).utc().format(
+                            // data: 'start_time'
+                            data: {
+                                display: (data) => {
+                                    return data.start_time === null ? '' : moment(data.start_time).utc().format(
                                         sovtimerSettings.datetimeFormat.datetimeLong
                                     );
                                 },
                                 sort: (data) => {
-                                    return data === null ? '' : data;
+                                    return data.start_time === null ? '' : data.start_time;
+                                },
+                                filter: (data) => {
+                                    return data.start_time === null ? '' : data.start_time;
+                                }
+                            }
+                        },
+
+                        // Column: 6
+                        {
+                            // data: 'remaining_time'
+                            data: {
+                                display: (data) => {
+                                    return data.remaining_time;
+                                },
+                                sort: (data) => {
+                                    return parseInt(data.remaining_time_in_seconds, 10);
+                                },
+                                filter: (data) => {
+                                    return parseInt(data.remaining_time_in_seconds, 10);
                                 }
                             }
                         },
 
                         // Column: 7
                         {
-                            data: 'remaining_time'
+                            // data: 'campaign_progress'
+                            data: {
+                                display: (data) => {
+                                    return data.campaign_progress;
+                                },
+                                sort: (data) => {
+                                    return data.campaign_progress;
+                                },
+                                filter: (data) => {
+                                    return data.active_campaign;
+                                }
+                            }
                         },
-
-                        // Column: 8
-                        {
-                            data: 'campaign_progress'
-                        },
-
-                        // Hidden columns
-                        // Column: 9
-                        {
-                            data: 'remaining_time_in_seconds'
-                        },
-
-                        // Column: 10
-                        {
-                            data: 'solar_system_name'
-                        },
-
-                        // Column: 11
-                        {
-                            data: 'constellation_name'
-                        },
-
-                        // Column: 12
-                        {
-                            data: 'region_name'
-                        },
-
-                        // Column: 13
-                        {
-                            data: 'defender_name'
-                        },
-
-                        // Column: 14
-                        {
-                            data: 'active_campaign'
-                        }
                     ],
                     columnDefs: [
                         {
-                            visible: false,
-                            targets: [0, 9, 10, 11, 12, 13, 14]
+                            targets: [4, 5, 6, 7],
+                            columnControl: _removeSearchFromColumnControl(sovtimerSettings.dataTables.columnControl, 1)
                         },
-                        // {
-                        //     width: '115px',
-                        //     targets: [7]
-                        // },
                         {
-                            width: '150px',
-                            targets: [8]
+                            target: 6,
+                            type: 'string',
+                            width: 175
+                        },
+                        {
+                            target: 7,
+                            type: 'string',
+                            width: 175
                         }
                     ],
-                    order: [[6, 'asc']],
-                    filterDropDown: {
-                        columns: [
-                            // Filter: Type
-                            // {
-                            //     idx: 0,
-                            //     title: sovtimerSettings.translation.dtFilter.type
-                            // },
-
-                            // Filter: System
-                            {
-                                idx: 10,
-                                title: sovtimerSettings.translation.dtFilter.system
-                            },
-
-                            // Filter: Constellation
-                            {
-                                idx: 11,
-                                title: sovtimerSettings.translation.dtFilter.constellation
-                            },
-
-                            // Filter: Region
-                            {
-                                idx: 12,
-                                title: sovtimerSettings.translation.dtFilter.region
-                            },
-
-                            // Filter: Owner/Defender
-                            {
-                                idx: 13,
-                                title: sovtimerSettings.translation.dtFilter.owner
-                            },
-
-                            // Filter: Active Campaign
-                            {
-                                idx: 14,
-                                title: sovtimerSettings.translation.dtFilter.activeCampaign
-                            }
-                        ],
-                        autoSize: false,
-                        bootstrap: true,
-                        bootstrap_version: 5
-                    },
+                    order: [[5, 'asc']],
                     createdRow: (row, data) => {
                         // Increment total timer
                         elements.campaignsTotal.html(parseInt(elements.campaignsTotal.html()) + 1);
