b6f1cc981b9ef7aa435ca5b0abc9c960ee4f684e
Peter Pfeufer <info@ppfeufer.de>
2025-11-14 23:26:54 +0100

    [RELEASE] v3.0.1
    ## [3.0.1] - 2025-11-14
    
    ### Added
    
    - ESI handler to handle ESI calls and deduplicate code
    
    ### Changed
    
    - Allow for public access if enabled in settings
    - Improved JS performance when recalculating remaining time for campaigns
    - Improvement to the campaign table

--- sovtimer/__init__.py
+++ sovtimer/__init__.py
@@ -5,7 +5,7 @@ App init
 # Django
 from django.utils.translation import gettext_lazy as _
 
-__version__ = "3.0.0"
+__version__ = "3.0.1"
 __title__ = "Sovereignty Timer"
 __title_translated__ = _("Sovereignty Timer")
 
